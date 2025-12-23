c6dfac81ee34b83a3a3ef2c589bb31ab3fa26bbb
raisircl <rai.verma@live.com>
2025-12-23 12:14:23 +0530

    mvc explaination

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProjectDetails()
+        {
+            return View();
+        }
+    }
+}
